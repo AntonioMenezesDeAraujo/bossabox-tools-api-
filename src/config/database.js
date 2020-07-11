63c8bf2e1f1f0225154117a9b8bbc6a9f0ffdcde
AntonioMAraujo <netynho_antonio@hotmail.com>
2020-07-11 09:01:05 -0300

    correção para adicionar o postgres

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
     dialect: 'postgres',
-    host: 'localhost',
+    host: 'postgresql-slippery-22976',
     username: 'postgres',
     password: 'postgres',
     database: 'desafio_bossa_box',
