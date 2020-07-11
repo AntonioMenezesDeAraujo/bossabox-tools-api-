3f0c5efd43f675f6ec6470fd53d806e336c57cb4
AntonioMAraujo <netynho_antonio@hotmail.com>
2020-07-11 09:07:11 -0300

    correção para adicionar o postgres

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: 'postgres',
-    database: 'desafio_bossa_box',
+    host: 'ec2-18-214-119-135.compute-1.amazonaws.com',
+    username: 'naonbgnwbkaosl',
+    password: '59589def0625ffbd46aae0913a216f1a13814c605190aa3f0b212f7594138250',
+    database: 'd57q7n9o5fdbvo',
     define: {
         timestamps: true,
         underscored: true
